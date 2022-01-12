048a9ce1897f8e8a02ba16b544db2a32edca6687
Bhupesh Parab - NeoSoft (Partner <P115552@fullertonindia.local>
2022-01-12 11:17:47 +0530

    initial commit

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRVMFWfoV5vgEI82Qy0jbIOT-q9ZyTWTQ",
+    authDomain: "csp-fcm.firebaseapp.com",
+    projectId: "csp-fcm",
+    storageBucket: "csp-fcm.appspot.com",
+    messagingSenderId: "1063903322339",
+    appId: "1:1063903322339:web:72f81dfb12fbb4c991a5d6"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
